40ceda605cfb2809dd87dafa8cc5864dcafa4eec
Adrian Flores <A20203669@alumnos.uady.mx>
2025-03-24 15:26:11 -0600

    Reconfiguración de los parametros para la pantalla

--- main/test.c
+++ main/test.c
@@ -4,30 +4,37 @@
 #include <hagl_hal.h>
 #include <hagl.h>
 #include "sdkconfig.h"
+#include <wchar.h>
 
-#define WIDTH 128
-#define HEIGHT 160
+// Fuente
+#include <font6x9.h>
 
+// Colores
+#define blanco 0xffff
+#define cyan 0x9c3d
+
+/* Nada mas por referencia...
 #define PIN_NUM_MOSI 23
 #define PIN_NUM_CLK  18
 #define PIN_NUM_CS   21
 #define PIN_NUM_DC   22
 #define PIN_NUM_RST  19
+*/
 
 static hagl_backend_t *display;
 
 struct {
-	uint8_t x, y, dx, dy;
+	int16_t x, y, dx, dy;
 } ball = {};
 
 
 void physics() {
 	ball.x += ball.dx;
 	ball.y += ball.dy;
-	if(ball.x >= WIDTH || ball.x <= 0) {
+	if(ball.x >= display->width || ball.x <= 0) {
 		ball.dx *= -1;
 	}
-	if(ball.y >= HEIGHT || ball.y <= 0) {
+	if(ball.y >= display->height || ball.y <= 0) {
 		ball.dy *= -1;
 	}
 }
@@ -35,15 +42,23 @@ void physics() {
 void app_main(void) {
 	display = hagl_init();
 
-	ball.x = WIDTH / 2;
-	ball.y = HEIGHT / 2;
+	ball.x = display->width / 2;
+	ball.y = display->height / 2;
 	ball.dx = 5;
 	ball.dy = 5;
 
+	wchar_t texto[12];
+	uint8_t textoX, textoY;
+	textoX = (display -> width / 2) - (6 * 3);
+	textoY = (display -> height / 2) - 9;
+
 	while(true) {
 		hagl_clear(display);
-		hagl_fill_circle(display, ball.x, ball.y, 10, 0xffff);
+		swprintf(texto, 12, u"%i,%i", ball.x, ball.y);
+		hagl_put_text(display, texto, textoX, textoY, cyan, font6x9);
+		hagl_fill_circle(display, ball.x, ball.y, 10, blanco);
 		physics();
+		hagl_flush(display);
 		vTaskDelay(pdMS_TO_TICKS(100));
 	}
 
